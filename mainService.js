773ed678752a1967842c0c0fd16c8b65667aba61
hanthom <m.hantho@gmail.com>
2015-11-16 16:02:34 -0600

    added code for GET call

--- mainService.js
+++ mainService.js
@@ -1,5 +1,8 @@
-myBreweryList.service('mainService', function() {
+myBreweryList.service('mainService', function($http) {
 
-
+	this.getBrewery = function(search) {
+		method: 'GET',
+		url: "http://api.brewerydb.com/v2/?key=b124abbace7776c29aa83a02dc882e92" + search,
+	};
 	
 })
