ec1fa189c5abc18b05c5c13724533eb77b2617c9
hanthom <m.hantho@gmail.com>
2015-11-16 16:11:06 -0600

    Finished GET call and added input on index

--- mainService.js
+++ mainService.js
@@ -2,7 +2,7 @@ myBreweryList.service('mainService', function($http) {
 
 	this.getBrewery = function(search) {
 		method: 'GET',
-		url: "http://api.brewerydb.com/v2/?key=b124abbace7776c29aa83a02dc882e92" + search,
+		url: "http://api.brewerydb.com/v2/?key=b124abbace7776c29aa83a02dc882e92&type=brewery&q=" + search,
 	};
 	
 })
