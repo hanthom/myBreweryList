3c54529d82995ecf926a8372ff085c65d9ed6480
hanthom <m.hantho@gmail.com>
2015-11-16 14:31:02 -0600

    Home folder filled out with files

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 var myBreweryList = angular.module('myBreweryList',['ui.router']);
 
+myBreweryList.config(function ($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			controller: 'homeCtrl',
+			templateUrl: 'home/homeTmpl.html',
+		});
+		// .state('search', {
+		// 	url: '/search',
+		// })
+
+
+})
---
+++ home/homeService.js
@@ -0,0 +1,5 @@
+myBreweryList.service('homeService', function($http) {
+
+	
+
+})
