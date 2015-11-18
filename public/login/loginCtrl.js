2998d171a3b8f15ac4ba9d191f0c9e61355c6564
hanthom <m.hantho@gmail.com>
2015-11-18 17:19:17 -0600

    Profile page now works

--- public/login/loginCtrl.js
+++ public/login/loginCtrl.js
@@ -3,7 +3,7 @@ myBreweryList.controller('loginCtrl', function($scope, userService, $state) {
 	$scope.login = function() {
 		return userService.login($scope.user).then(function() {
 			console.log("Login successful")
-			$state.go('profile');
+			$state.go('home');
 		}).catch(function(err) {
 			$scope.error = "Login Error: " + err;
 		})
--- public/profile/profileCtrl.js
+++ public/profile/profileCtrl.js
@@ -1,11 +1,15 @@
-myBreweryList.controller('profileCtrl', function($scope, $firebaseObject) {
+myBreweryList.controller('profileCtrl', function($scope, $firebaseObject, $firebaseArray) {
+
 
 	if ($scope.user) {
-		var profileRef = new Firebase("https://mybrewerylist.firebaseio.com/users/" + $scope.user.id);
+		var baseURL = "https://mybrewerylist.firebaseio.com/users/" + $scope.user.id;
+		var profileRef = new Firebase(baseURL);
 		$scope.userProfile = $firebaseObject(profileRef);
+
+		var favoritesRef = new Firebase(baseURL + '/favorites');
+		$scope.favorites = $firebaseArray(favoritesRef);
+		console.log($scope.favorites);
 	}
-	
-	$scope.favorites = $scope.userProfile.favorites;
 
 
 
