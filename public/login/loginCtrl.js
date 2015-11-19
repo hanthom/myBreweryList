dbcb3dff025e26c50eccbf1beda304befedba0fb
hanthom <m.hantho@gmail.com>
2015-11-19 08:27:55 -0600

    Minor updates

--- public/login/loginCtrl.js
+++ public/login/loginCtrl.js
@@ -2,7 +2,8 @@ myBreweryList.controller('loginCtrl', function($scope, userService, $state) {
 
 	$scope.login = function() {
 		return userService.login($scope.user).then(function() {
-			console.log("Login successful")
+			console.log("Login successful");
+
 			$state.go('home');
 		}).catch(function(err) {
 			$scope.error = "Login Error: " + err;
--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -1,27 +1,21 @@
 myBreweryList.controller('mainCtrl', function($scope, mainService, userService, $firebaseObject) {
 
 	var auth = userService.getAuthObj();
+	$scope.loggedIn = true;
 
 	auth.$onAuth(function(userAuth) {
 		if (userAuth) {
 			var userRef = new Firebase("https://mybrewerylist.firebaseio.com/users/" + userAuth.uid);
 			$scope.user = $firebaseObject(userRef);
+			
 		}
 		else {
 			$scope.user = userAuth;
 		}
-
-		// if (userAuth) {
-		// 	var profileRef = new Firebase("https://mybrewerylist.firebaseio.com/users/" + userAuth.id);
-		// 	$scope.profileList = $firebaseArray(profileRef);
-		// }
-		// else {
-		// 	$scope.profileList;
-		// }
 	})
 
-	$scope.logout = function(user) {
-		userService.logout();
-	}
+	// $scope.logout = function(user) {
+	// 	userService.logout();
+	// }
 
 })
