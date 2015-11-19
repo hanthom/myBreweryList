9702f3aa5f9c0cb316cd5d239a4e3c63a0553126
hanthom <m.hantho@gmail.com>
2015-11-19 09:19:19 -0600

    Updated minor html and css styling

--- public/login/loginCtrl.js
+++ public/login/loginCtrl.js
@@ -3,7 +3,6 @@ myBreweryList.controller('loginCtrl', function($scope, userService, $state) {
 	$scope.login = function() {
 		return userService.login($scope.user).then(function() {
 			console.log("Login successful");
-
 			$state.go('home');
 		}).catch(function(err) {
 			$scope.error = "Login Error: " + err;
